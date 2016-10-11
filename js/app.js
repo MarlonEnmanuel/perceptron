c39625ce994d410bf68374e5454c084c333e8010
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2016-10-11 00:37:31 -0500

    Vista terminada

--- js/app.js
+++ js/app.js
@@ -3,16 +3,19 @@ window.Perceptron = {
 	init : function(){
 		var self = this;
 
-		if(!self.load()){
+		if(!self.tmpl)
+			self.tmpl = $('#tmpl-muestra').text();
+
+		if(self.load() == false){
 			self.data = {
 				nombre : 'Nuevo Perceptrón',
 				razon : 0.9,
 				muestras : [],
 				evaluado : false,
 				pesos : {
-					w1 : ((Math.random()*2)-1),
-					w2 : ((Math.random()*2)-1),
-					w3 : ((Math.random()*2)-1),
+					w1 : Math.round(((Math.random()*2)-1)*100)/100,
+					w2 : Math.round(((Math.random()*2)-1)*100)/100,
+					w3 : Math.round(((Math.random()*2)-1)*100)/100,
 				}
 			};
 			self.saved = false;
@@ -23,8 +26,8 @@ window.Perceptron = {
 
 	load : function(){
 		var self = this;
-		if(!localStorage.data) 
-			return false;
+
+		if(!localStorage.data) return false;
 
 		self.data = JSON.parse(localStorage.data);
 		Materialize.toast('Datos Cargados', 2000);
@@ -66,7 +69,15 @@ window.Perceptron = {
 		var self = this;
 		$('#app-nombre').text(self.data.nombre);
 		$('#app-razon').text(self.data.razon);
-		$('select').material_select();
+
+		$('#app-recta-w1').text(self.data.pesos.w1);
+		$('#app-recta-w2').text(self.data.pesos.w2);
+		$('#app-recta-w3').text(self.data.pesos.w3);
+
+		$('#app-muestras').empty();
+		self.data.muestras.forEach(function(el, i){
+			self.renderMuestra(el).prependTo('#app-muestras');
+		});
 	},
 
 	restart : function(){
@@ -75,6 +86,42 @@ window.Perceptron = {
 			localStorage.clear();
 			self.init();
 		}
+	},
+
+	addMuestra : function(){
+		var self = this;
+
+		var inp = window.prompt('Ingrese muestra: Ej: 0.3,0.63,A');
+		if(!inp) return;
+
+		var inp = inp.split(',');
+		if(inp.length!=3){
+			Materialize.toast('Formato de muestra incorrecto', 1500); 
+			return;
+		}
+
+		var m = {
+			x : parseFloat(inp[0].trim()),
+			y : parseFloat(inp[1].trim()),
+			clase : inp[2].trim().toUpperCase(),
+		};
+
+		self.data.muestras.push(m);
+		self.render();
+	},
+
+	renderMuestra : function(muestra){
+		var self = this;
+		var $m = $(self.tmpl);
+		$m.find('.muestra-x').text(muestra.x);
+		$m.find('.muestra-y').text(muestra.y);
+		$m.find('.muestra-clase').text(muestra.clase);
+		$m.find('.muestra-quitar').click(function(ev){
+			ev.preventDefault();
+			$m.remove();
+			self.render();
+		});
+		return $m;
 	},
 
 };

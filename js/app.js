2edd702e63fceb157d4902f62488a49b7c7ccc70
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2016-10-11 12:42:41 -0500

    Perceptron terminado

--- js/app.js
+++ js/app.js
@@ -11,16 +11,33 @@ window.Perceptron = {
 				nombre : 'Nuevo Perceptrón',
 				razon : 0.9,
 				muestras : [],
-				evaluado : false,
-				pesos : {
-					w1 : Math.round(((Math.random()*2)-1)*100)/100,
-					w2 : Math.round(((Math.random()*2)-1)*100)/100,
-					w3 : Math.round(((Math.random()*2)-1)*100)/100,
-				}
+				entrenado : false,
+				pesos : self.getPesos(),
 			};
 			self.saved = false;
 		}
 
+		self.render();
+	},
+
+	getPesos : function(){
+		var getRandom = function(){
+			var n = Math.round((Math.random()-0.5)*100)/100;
+			if(n == 0) return getRandom();
+			return n;
+		}
+		return {
+			w1 : getRandom(),
+			w2 : getRandom(),
+			w3 : getRandom(),
+		}
+	},
+
+	nuevosPesos : function(){
+		var self = this;
+		self.data.pesos = self.getPesos();
+		self.saved = false;
+		self.data.entrenado = false;
 		self.render();
 	},
 
@@ -123,5 +140,87 @@ window.Perceptron = {
 		});
 		return $m;
 	},
+
+
+	entrenar : function(){
+		var self = this;
+
+		if(self.data.muestras.length<3){
+			Materialize.toast('Debe tener mínimo 3 muestras', 2000);
+			return;
+		}
+
+		$('#app-addMuestra').attr('disabled', 'disabled');
+		$('#app-entrenar').attr('disabled', 'disabled');
+		$('#app-consultar').attr('disabled', 'disabled');
+
+		self.evaluar(0);
+	},
+
+	evaluar : function(i){
+		var self = this;
+
+		var m = self.data.muestras[i];
+		var w = self.data.pesos;
+		var r = self.data.razon;
+
+		m.c = (m.clase=='A') ? 0 : 1 ;
+
+		var net = (m.x)*(w.w1) + (m.y)*(w.w2) + (w.w3);
+		var d = (net>0) ? 1 : 0 ;
+
+		if(d != m.c){
+			//Corregir Pesos
+			w.w1 = w.w1 + (r*(m.c - d)*m.x);
+			w.w2 = w.w2 + (r*(m.c - d)*m.y);
+			w.w3 = w.w3 + (r*(m.c - d)*1);
+
+			self.data.pesos = w;
+		}
+
+		i++;
+		if(i < self.data.muestras.length){
+			self.render();
+			window.setTimeout(function(){
+				self.evaluar(i);
+			}, 1000);
+		}else{
+			$('#app-addMuestra').removeAttr('disabled');
+			$('#app-entrenar').removeAttr('disabled');
+			$('#app-consultar').removeAttr('disabled');
+			Materialize.toast('Entrenamiento correto', 3000);
+			self.data.entrenado = true;
+		}
+	},
+
+	consultar : function(){
+		var self = this;
+
+		if(!self.data.entrenado){
+			Materialize.toast('Debe entrenar primero');
+			return;
+		}
+
+		var inp = window.prompt('Ingrese coordenada: Ej: -3,4.5');
+		if(!inp) return;
+
+		var inp = inp.split(',');
+		if(inp.length!=2){
+			Materialize.toast('Formato de coordenada incorrecto', 1500); 
+			return;
+		}
+
+		var c = {
+			x : parseFloat(inp[0].trim()),
+			y : parseFloat(inp[1].trim()),
+		};
+
+		var w = self.data.pesos;
+		var net = (c.x)*(w.w1) + (c.y)*(w.w2) + (w.w3);
+		debugger;
+		var d = (net>0) ? 'B' : 'A' ;
+		
+		alert('La coordenada ('+c.x+','+c.y+') es de clase '+d);
+	}
 
 };
